287ee2cba121a83571838d0772e3be3121b9ec98
MattRyan <matt@ryan-realty.com>
2026-03-09 12:05:49 -0700

    Project foundation: dependencies, Inngest, Sentry, master plan, agent configs

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://d56b8b430cfda8e7fd5476e964caa2f1@o4511015526203392.ingest.us.sentry.io/4511015954284544",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
